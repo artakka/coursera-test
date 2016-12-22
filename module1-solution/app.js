21d1ba0c8cf092cbeec8f88987befc067d064bc2
Artak Kalantarian <artak@burlymarmot.com>
2016-12-21 20:47:57 -0800

    Put the border around the right object

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,50 +1,52 @@
 (function () {
-'use strict';
+  'use strict';
 
-angular.module('LunchCheckerApp', [])
+  angular.module('LunchCheckerApp', [])
 
-.controller('LunchCheckerController', LunchCheckerController);
+  .controller('LunchCheckerController', LunchCheckerController);
 
-LunchCheckerController.$inject = ['$scope'];
+  LunchCheckerController.$inject = ['$scope'];
 
-function LunchCheckerController ($scope) {
-  $scope.message = "";
-  $scope.dishList = "";
-  $scope.textStyle={};
+  function LunchCheckerController ($scope) {
+    $scope.message = "";
+    $scope.dishList = "";
+    $scope.textStyle={};
+    $scope.msgStyle={};
 
-  $scope.updateMessage = function () {
+    $scope.updateMessage = function () {
 
+      var itemCount = calcCount($scope.dishList);
 
-    var itemCount = calcCount($scope.dishList);
-
-    if(itemCount == 0)  {
-        $scope.message = "Please enter data first"
-        $scope.textStyle.style = {"color":"red", "border":"1px solid red"};
-    }
-    else
-    {
-      $scope.textStyle.style = {"color":"green", "border":"1px solid green"};
-
-      if(itemCount > 3)
-      {
-        $scope.message = "Too much!"
-      } else {
-        $scope.message = "Enjoy!"
+      if(itemCount == 0)  {
+          $scope.textStyle.style = {"border":"2px solid red"};
+          $scope.msgStyle.style = {"color":"red"};
+          $scope.message = "Please enter data first"
       }
-    }
-};
+      else
+      {
+        $scope.textStyle.style = {"border":"2px solid green"};
+        $scope.msgStyle.style = {"color":"green"};
 
-function calcCount(itemsStr)
-{
-  var splitArray = itemsStr.split(",");
-  var filteredSplitArray = splitArray.filter(
-    function(n){
-      return n.trim() != ""
-    }
-  );
+        if(itemCount > 3)
+        {
+          $scope.message = "Too much!"
+        } else {
+          $scope.message = "Enjoy!"
+        }
+      }
+  };
 
-  return filteredSplitArray.length;
-}
+  function calcCount(itemsStr)
+  {
+    var splitArray = itemsStr.split(",");
+    var filteredSplitArray = splitArray.filter(
+      function(n){
+        return n.trim() != ""
+      }
+    );
+
+    return filteredSplitArray.length;
+  }
 
 }
 
